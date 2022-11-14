464ddda346e0aef6546785000d7f19c95cc24810
Rasmus S <rasmus.salmela@gmail.com>
2022-11-14 21:57:15 +0200

    viikko 2

---
+++ verkkokauppa-1/src/pankki.py
@@ -0,0 +1,17 @@
+from kirjanpito import the_kirjanpito_olio
+
+
+class Pankki:
+
+    def __init__(self, kp=the_kirjanpito_olio):
+        self._kirjanpito = kp
+
+    def tilisiirto(self, nimi, viitenumero, tililta, tilille, summa):
+        self._kirjanpito.lisaa_tapahtuma(
+            f"tilisiirto: tililtä {tililta} tilille {tilille} viite {viitenumero} summa {summa}e"
+        )
+
+        # täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
+        return True
+
+the_pankki_olio = Pankki()
