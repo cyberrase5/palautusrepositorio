8504f2b01a13d838e0def2d2952c00bc6a4e6976
Rasmus S <rasmus.salmela@gmail.com>
2022-11-27 19:04:08 +0200

    Test hinta ja tavatoiden määrä

---
+++ tdd-ostoskori/src/ostoskori.py
@@ -0,0 +1,45 @@
+from tuote import Tuote
+from ostos import Ostos
+
+class Ostoskori:
+    def __init__(self):
+        pass
+        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+        self.tuotteet = []
+
+    def tavaroita_korissa(self):
+        # kertoo korissa olevien tavaroiden lukumäärän
+        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
+        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
+
+        count = 0
+        for item in self.tuotteet:
+            count += item.lukumaara
+
+        return count
+
+    def hinta(self):
+        # kertoo korissa olevien ostosten yhteenlasketun hinnan
+
+        count = 0
+        for item in self.tuotteet:
+            count += item.hinta
+
+        return count
+
+    def lisaa_tuote(self, lisattava: Tuote):
+        # lisää tuotteen
+        pass
+
+    def poista_tuote(self, poistettava: Tuote):
+        # poistaa tuotteen
+        pass
+
+    def tyhjenna(self):
+        pass
+        # tyhjentää ostoskorin
+
+    def ostokset(self):
+        pass
+        # palauttaa listan jossa on korissa olevat ostos-oliot
+        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
